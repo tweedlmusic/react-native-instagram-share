3d2a67073b4bf76df16847dc473a4e0bcc00d606
Melody Morgan <doremelody@gmail.com>
2018-07-10 14:22:33 -0700

    deprecated override

--- android/src/main/java/com/RNInstagramShare/RNInstagramSharePackage.java
+++ android/src/main/java/com/RNInstagramShare/RNInstagramSharePackage.java
@@ -16,7 +16,7 @@ public class RNInstagramSharePackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new RNInstagramShareModule(reactContext));
     }
 
-    @Override
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
